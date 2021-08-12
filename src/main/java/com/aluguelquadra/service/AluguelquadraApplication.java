84005b6b206e576e021552fc2960c42721642f75
William Bohn <williamxavero@gmail.com>
2021-08-12 18:30:25 -0300

    Setup do projeto

---
+++ src/main/java/com/aluguelquadra/service/AluguelquadraApplication.java
@@ -0,0 +1,13 @@
+package com.aluguelquadra.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluguelquadraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluguelquadraApplication.class, args);
+	}
+
+}
