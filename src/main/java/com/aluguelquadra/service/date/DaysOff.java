cad5963f3317af1c9969b2d33d7d0ed4c84effa2
williamxave <williamxave@gmail.com>
2021-08-16 17:30:54 -0300

    Listando dias uteis do mes

--- src/main/java/com/aluguelquadra/service/date/DaysOff.java
+++ src/main/java/com/aluguelquadra/service/date/DaysOff.java
@@ -2,15 +2,29 @@ package com.aluguelquadra.service.date;
 
 import org.springframework.stereotype.Component;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
+import java.time.YearMonth;
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class DaysOff {
 
+    private List<LocalDate> daysOff  = new ArrayList<>();
+
     public List<LocalDate> showDaysOffs(){
+        LocalDate  dayNow = LocalDate.now();
 
-        return  null;
+        YearMonth yearMonth = YearMonth.of(dayNow.getYear(), dayNow.getMonth());
 
+        for(int day = 1; day < yearMonth.lengthOfMonth(); day++){
+            LocalDate dates = yearMonth.atDay(day);
+
+            if(!dates.getDayOfWeek().equals(DayOfWeek.SUNDAY) &&!dates.getDayOfWeek().equals(DayOfWeek.SATURDAY)){
+                daysOff.add(dates);
+            }
+        }
+        return daysOff;
     }
 }
---
+++ src/main/java/com/aluguelquadra/service/date/ShowDateOffController.java
@@ -0,0 +1,26 @@
+package com.aluguelquadra.service.date;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@RestController
+@RequestMapping("api/date")
+public class ShowDateOffController {
+
+    private DaysOff daysOff;
+
+    public ShowDateOffController(DaysOff daysOff) {
+        this.daysOff = daysOff;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<LocalDate>> showDayOff(){
+        var days = daysOff.showDaysOffs();
+        return ResponseEntity.ok(days);
+    }
+}
