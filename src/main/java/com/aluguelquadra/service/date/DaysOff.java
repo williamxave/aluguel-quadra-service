7c07737c4bb573274cf6d001a6fd5c8cbdc9e999
williamxave <williamxave@gmail.com>
2021-08-16 16:54:30 -0300

    Criacao da classe day

---
+++ src/main/java/com/aluguelquadra/service/date/DaysOff.java
@@ -0,0 +1,16 @@
+package com.aluguelquadra.service.date;
+
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Component
+public class DaysOff {
+
+    public List<LocalDate> showDaysOffs(){
+
+        return  null;
+
+    }
+}
